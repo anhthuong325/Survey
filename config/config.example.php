57a4b41c2bb1d667cb4666b5a36296dff244c858
Thuong Le <thuongla@imt-soft.com>
2022-10-31 20:34:59 +0700

    no message

--- config/config.example.php
+++ config/config.example.php
@@ -4,4 +4,5 @@
     define('DB_NAME','survey');
     define('DB_HOST','localhost');
     define('DB_PORT',3306);
+    //không được thay đổi file config.example.php, chỉ được copy file này và đổi tên thành config.php mà dùng
 ?>
