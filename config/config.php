d828083e06365698a1404cf8aa05972c0d2b63d8
Thuong Le <thuongla@imt-soft.com>
2022-10-31 20:32:10 +0700

    change như cũ

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+    define('DB_USER','root');
+    define('DB_PASSWORD','');
+    define('DB_NAME','survey');
+    define('DB_HOST','localhost');
+    define('DB_PORT',3306);
+?>
--- utils/mysqldump.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-"C:\Program Files\MySQL\MySQL Workbench 8.0 CE\mysqldump.exe"%* --column--statistics=0
