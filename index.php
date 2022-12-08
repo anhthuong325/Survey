ca95b4b7194f103727d617e89a2a746ef122cfd1
LOANPIA\phucl <phucloan0801@gmail.com>
2022-12-08 21:31:57 +0700

    complete content for adminpage

--- index.php
+++ index.php
@@ -7,5 +7,252 @@ if (!in_array($_SESSION['ROLE'], array(UserType::ADMIN, UserType::STUDENT))) {
     die();
 }
 ?>
-<li class="nav-item"><a class="nav-link" href="./login.php?logout=true"><i class="fa fa-sign-out"></i> Logout</a></li>
-<h1>Page admin</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!--important link source from "https://bootstrapious.com/p/about-us-page"-->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/main2.css">    
+
+    <link rel="home" href="https://mazipan.github.io/bootstrap4-admin-dashboard-template/">
+    <link rel="icon" type="image/png" sizes="16x16" href="https://mazipan.github.io/favicon/favicon-16x16.png">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-25065548-2"></script>
+    <script>window.dataLayer = window.dataLayer || []; function gtag() { dataLayer.push(arguments); } gtag('js', new Date()); gtag('config', 'UA-25065548-2');</script>
+
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+    (adsbygoogle = window.adsbygoogle || []).push({
+        google_ad_client: "ca-pub-5442972248172818",
+        enable_page_level_ads: true
+    });
+    </script>
+
+</head>
+<body>
+<?php include 'views/layouts/page_header.php';?>
+
+  <div class="container-fluid">
+    <div class="row">
+      <aside class="col-md-2 d-none d-md-block bg-light sidebar">
+        <div class="sidebar-sticky">
+
+          <h6 class="sidebar-heading">
+            <span>Danh mục chính</span>
+          </h6>
+
+          <ul class="nav flex-column">
+            <li class="nav-item">
+              <a class="nav-link active" href="#">
+                <i class="fa fa-tachometer"></i>
+                Dashboard - Quản lý hệ thống
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="forms.html">
+                <i class="fa fa-pencil-square-o"></i> Biểu mẫu khảo sát
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="ui-elements.html">
+                <i class="fa fa-desktop"></i> Khảo sát
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="tables.html">
+                <i class="fa fa-table"></i> Câu hỏi
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="presentations.html">
+                <i class="fa fa-bar-chart"></i> Thống kê phản hồi
+              </a>
+            </li>
+          </ul>
+
+          <h6 class="sidebar-heading">
+            <span>Danh mục người dùng</span>
+          </h6>
+
+          <ul class="nav flex-column">
+                <a class="nav-link" href="#">
+                    <i class="fa fa-user-o"></i> Hồ sơ thông tin</i>
+                </a>
+            <li class="nav-item"><a class="nav-link" href="./login.php?logout=true"><i class="fa fa-sign-out"></i> Logout</a></li>
+          </ul>
+
+        </div>
+      </aside>
+      <main class="col-md-10 ml-sm-auto col-lg-10 pt-3 px-4">
+
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+          <h1 class="h2"><i class="fa fa-tachometer"></i> Dashboard</h1>
+          <div class="btn-toolbar mb-2 mb-md-0">
+            <button class="btn btn-sm btn-primary dropdown-toggle">
+              <i class="fa fa-calendar"></i>
+              This week
+            </button>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-3 col-md-3 col-sm-12 pr-0 mb-3">
+            <div class="card text-white bg-primary">
+              <div class="card-header"><i class="fa fa-shopping-bag"></i> New Orders</div>
+              <div class="card-body">
+                <h3 class="card-title">150</h3>
+              </div>
+              <a class="card-footer text-right" href="#">
+                More info <i class="fa fa-arrow-circle-right"></i>
+              </a>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-3 col-sm-12 pr-0 mb-3">
+            <div class="card text-white bg-success">
+              <div class="card-header"><i class="fa fa-bar-chart"></i> Bounce Rate</div>
+              <div class="card-body">
+                <h3 class="card-title">53%</h3>
+              </div>
+              <a class="card-footer text-right" href="#">
+                More info <i class="fa fa-arrow-circle-right"></i>
+              </a>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-3 col-sm-12 pr-0 mb-3">
+            <div class="card text-white bg-warning">
+              <div class="card-header"><i class="fa fa-user-plus"></i> User Registrations</div>
+              <div class="card-body">
+                <h3 class="card-title">44</h3>
+              </div>
+              <a class="card-footer text-right" href="#">
+                More info <i class="fa fa-arrow-circle-right"></i>
+              </a>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-3 col-sm-12 pr-0 mb-3">
+            <div class="card text-white bg-danger">
+              <div class="card-header"><i class="fa fa-pie-chart"></i> Unique Visitor</div>
+              <div class="card-body">
+                <h3 class="card-title">65</h3>
+              </div>
+              <a class="card-footer text-right" href="#">
+                More info <i class="fa fa-arrow-circle-right"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6 col-md-6 col-sm-12 pr-0 mb-3">
+            <div class="card-collapsible card">
+              <div class="card-header">
+                Table <i class="fa fa-caret-down caret"></i>
+              </div>
+              <div class="card-body">
+                <table class="table">
+                  <thead class="thead bg-primary text-white">
+                    <tr>
+                      <th scope="col">#</th>
+                      <th scope="col">First</th>
+                      <th scope="col">Last</th>
+                      <th scope="col">Handle</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th scope="row">1</th>
+                      <td>Mark</td>
+                      <td>Otto</td>
+                      <td>@mdo</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">2</th>
+                      <td>Jacob</td>
+                      <td>Thornton</td>
+                      <td>@fat</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">3</th>
+                      <td>Larry</td>
+                      <td>the Bird</td>
+                      <td>@twitter</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12 pr-0 mb-3">
+            <div class="card-collapsible card">
+              <div class="card-header">
+                Quick Form <i class="fa fa-caret-down caret"></i>
+              </div>
+              <div class="card-body">
+                <form>
+
+                  <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Assignee's email">
+                    <div class="input-group-append">
+                      <span class="input-group-text">@example.com</span>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Ticket title">
+                  </div>
+
+                  <div class="form-group">
+                    <textarea class="form-control" placeholder="Ticket description" cols="30" rows="5"></textarea>
+                  </div>
+
+                  <div class="form-group row">
+                    <div class="col-sm-10">
+                      <button type="submit" class="btn btn-primary">
+                        <i class="fa fa-send"></i>
+                        Submit Ticket
+                      </button>
+                    </div>
+                  </div>
+
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </main>
+    </div>
+  </div>
+
+   <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+    crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+    crossorigin="anonymous"></script>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.js"></script>
+  <script src="js/main.js"></script>
+  <script src="js/chart.js"></script>
+
+  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-5442972248172818" data-ad-slot="1487770485" data-ad-format="auto"></ins>
+  <script>
+  (adsbygoogle = window.adsbygoogle || []).push({});
+  </script>
+  
+  <script type="application/ld+json">{"@context":"http://schema.org","@type":"WebSite","url":"https://www.mazipan.github.io/","name":"Irfan Maulana | Front End Developer","author":"Irfan Maulana","image":"http://mazipan.github.io/images/irfan-maulana.jpg","description":"Irfan Maulana is Front End Developer from Indonesia - Man that craft some code to build a beauty and readable code, experienced in web and desktop technology.","sameAs":["https://www.facebook.com/mazipanneh","https://instagram.com/maz_ipan","https://twitter.com/Maz_Ipan","https://id.linkedin.com/in/irfanmaulanamazipan","https://www.slideshare.net/IrfanMaulana21","https://github.com/mazipan"]}</script>
+  <script type="application/ld+json">{"@context":"http://schema.org","@type":"Person","email":"mailto:mazipanneh@gmail.com","image":"http://mazipan.github.io/images/irfan-maulana.jpg","jobTitle":"Software Engineer","name":"Irfan Maulana","url":"https://www.mazipan.github.io/","sameAs":["https://www.facebook.com/mazipanneh","https://instagram.com/maz_ipan","https://twitter.com/Maz_Ipan","https://id.linkedin.com/in/irfanmaulanamazipan","https://www.slideshare.net/IrfanMaulana21","https://github.com/mazipan"]}</script>
+  <script type="application/ld+json">{"@context":"http://schema.org","@type":"BreadcrumbList","itemListElement":[{"@type":"ListItem","position":1,"item":{"@id":"http://mazipan.github.io/","name":"Home","image":"http://mazipan.github.io/images/irfan-maulana.jpg"}},{"@type":"ListItem","position":2,"item":{"@id":"http://mazipan.github.io/demo/","name":"Demo","image":"http://mazipan.github.io/images/irfan-maulana.jpg"}},{"@type":"ListItem","position":3,"item":{"@id":"http://mazipan.github.io/bootstrap4-admin-dashboard-template","name":"bootstrap4-admin-dashboard-template","image":"http://mazipan.github.io/images/irfan-maulana.jpg"]}</script>
+  <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+  <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+<?php include 'views/layouts/page_footer.php';?>
+</body>
+</html>
---
+++ views/layouts/page_header.php
@@ -0,0 +1,12 @@
+<header class="navbar navbar-expand sticky-top bg-primary navbar-dark flex-column flex-md-row bd-navbar">
+<a href="#" class="navbar-brand d-flex align-items-center"> <i class="fa fa-graduation-cap text-light mr-2"></i><strong>Quản lý khảo sát</strong></a>
+
+    <div class="navbar-nav-scroll">
+      <ul class="navbar-nav bd-navbar-nav flex-row">
+        <li class="nav-item px-2">
+          <a class="nav-link active" href="#">Trang Quản Trị</a>
+        </li>
+      </ul>
+    </div>
+
+</header>
